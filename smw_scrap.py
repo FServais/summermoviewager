31090b4f3ae0fdc0d3f94d9595500b8360265ec4
Fabrice Servais <fabrice.servais@gmail.com>
2018-06-26 10:45:03 +0200

    Clean comments + fix parsing bug

--- smw_scrap.py
+++ smw_scrap.py
@@ -12,27 +12,6 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Print iterations progress
-# def print_progress (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
-#     """
-#     Call in a loop to create terminal progress bar
-#     @params:
-#         iteration   - Required  : current iteration (Int)
-#         total       - Required  : total iterations (Int)
-#         prefix      - Optional  : prefix string (Str)
-#         suffix      - Optional  : suffix string (Str)
-#         decimals    - Optional  : positive number of decimals in percent complete (Int)
-#         length      - Optional  : character length of bar (Int)
-#         fill        - Optional  : bar fill character (Str)
-#     """
-#     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-#     filledLength = int(length * iteration // total)
-#     bar = fill * filledLength + '-' * (length - filledLength)
-#     print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
-#     # Print New Line on Complete
-#     if iteration == total:
-#         print()
-
-# Print iterations progress
 def print_progress(iteration, total, prefix='', suffix='', decimals=1, bar_length=100):
     """
     Call in a loop to create terminal progress bar
@@ -130,7 +109,7 @@ if __name__ == '__main__':
         for year in years:
             print_progress(progress, len(years), suffix='({})'.format(year), bar_length=50)
             page = SMWPage(year)
-            result += page.parse()
+            result = page.parse()
 
             writer.writerows(result)
 
